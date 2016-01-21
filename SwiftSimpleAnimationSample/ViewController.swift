93f045e96f74c80bdd0037eaff92f9ca4bb05ffb
Yutaka Tajika <y.tajika529@gmail.com>
2016-01-22 07:42:16 +0900

    Initial Commit

---
+++ SwiftSimpleAnimationSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftSimpleAnimationSample
+//
+//  Created by tajika on 2016/01/22.
+//  Copyright © 2016年 Tajika. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
